fe99ad380dcb5722ea7a1f2031a47e5f27240492
Deymeqpyra <deymecop@gmail.com>
2024-12-15 22:46:35 +0200

    feat: remake the tag and add tag button

--- src/project-managment-ui/src/features/Tags/Services/TagServices.tsx
+++ src/project-managment-ui/src/features/Tags/Services/TagServices.tsx
@@ -7,27 +7,22 @@ export class TagService {
     baseURL: "http://localhost:5134/tags",
   });
 
-  // Get all tags
   static async getAllTags(): Promise<TagDto[]> {
     return TagService.httpClient.get<TagDto[]>("/GetAllTags");
   }
 
-  // Add a new tag
   static async addTag(tagName: CreateTagDto): Promise<TagDto> {
     return TagService.httpClient.post<TagDto>("/AddTag", tagName);
   }
 
-  // Update an existing tag
   static async updateTag(tagId: string, tagName: CreateTagDto): Promise<TagDto> {
     return TagService.httpClient.put<TagDto>(`/UpdateTag/${tagId}`, tagName);
   }
 
-  // Get a tag by ID
   static async getTagById(tagId: string): Promise<TagDto> {
     return TagService.httpClient.get<TagDto>(`/GetTagById/${tagId}`);
   }
 
-  // Delete a tag
   static async deleteTag(tagId: string): Promise<void> {
     return TagService.httpClient.delete<void>(`/DeleteTag/${tagId}`);
   }
